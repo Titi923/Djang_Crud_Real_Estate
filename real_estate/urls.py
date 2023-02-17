74d8d1d2515278d9638277e1049e0185ab42aac7
titi923 <petrisor.buciuta92@gmail.com>
2023-02-17 14:52:50 +0200

    first commit

---
+++ real_estate/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from listings.views import (
+    listing_list, 
+    listing_retrieve, 
+    listing_create, 
+    listing_update,
+    listing_delete
+    )
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', listing_list),
+    path('listing/<pk>/', listing_retrieve),
+    path('listing/<pk>/edit/', listing_update),
+    path('listing/<pk>/delete/', listing_delete),
+    path('add-listing/', listing_create),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
